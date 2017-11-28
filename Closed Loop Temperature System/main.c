63acb3c428cd6c11f3b37719253e1fb91c898154
Russell Binaco <binacor8@students.rowan.edu>
2017-11-28 14:01:26 -0500

    code commented

--- Closed Loop Temperature System/main.c
+++ Closed Loop Temperature System/main.c
@@ -1,5 +1,6 @@
 #include <msp430.h> 
 
+//Declarations of all helper functions
 void setPWM(unsigned int duty);
 void setUpPWM();
 void pwmUARTInterrupt(long bufInput);
@@ -10,12 +11,20 @@ void timerA1Interrupt();
 void setUpTimerA0();
 void setDutyCycle(int temp);
 
+//Fields for average of 10 points
 int tempBuf[10];
 int tempBuf_index = 0;
 
+//fields for error multiplication constant and the goal temperature
 int k =5;
 long goaltemp = 40;
 
+/**
+* Closed-loop control program receives a goal temperature over uart, sets a PWM output to control a fan, and updates that PWM based on the current temperature of the voltage
+* regulator that the fan is cooling. 
+* @author Russell Binaco, Jake Fraser, Tanner Smith, Gray Thurston, Tomas Uribe
+* main sets up timers, UART communication and ADC channel, and initializes the duty cycle of the PWM. 
+**/
 void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
@@ -34,7 +43,11 @@ void main(void)
     }
 }
 
-
+/**
+* setUpPWM uses P2.1 as the PWM output pin
+* TA1CCR1/1000 is the duty cycle, initialized to 0.5 or 50%
+*
+**/
 void setUpPWM(){
     P2DIR |= BIT1;                            // P2.1 output
     P2SEL |= BIT1;                            // P2.1 for TA0 CCR1 Output Capture
@@ -45,6 +58,10 @@ void setUpPWM(){
     TA1CTL = TASSEL_2 + MC_1 + ID_0;                  // SMCLK, up mode, 1MhZ
 }
 
+/**
+* setUpUART uses 9600 BAUD rate, P1.1 for RX and P1.2 for TX. 
+*
+**/
 void setUpUART(){
     if (CALBC1_1MHZ==0xFF)                    // If calibration constant erased
     {
@@ -64,12 +81,21 @@ void setUpUART(){
 
 }
 
+/**
+* setUpADC selects A7, which is P1.7, and uses Vcc as reference. 
+*
+**/
 void setUpADC(){
     ADC10CTL1 = INCH_7 + SHS_1;               // P1.7, TA1 trigger sample start
     ADC10AE0 = 0x80;                          // P1.7 ADC10 option select
 
 }
 
+/**
+* setUpTimerA0: Timer A0 is the timer that controls UART sampling
+* in upmode, triggers at 2Hz. 
+*
+**/
 void setUpTimerA0(){
     TA0CCTL0 = CCIE;                           // Enable interrupt
     TA0CCR0 = 65534;                           // PWM Period
@@ -78,6 +104,12 @@ void setUpTimerA0(){
     TA0CTL = TASSEL_2 + MC_1 + ID_3;                  // ACLK, up mode
 }
 
+/**
+* setDutyCycle takes a goal temperature and uses a piecewise linear function to set the PWM to a corresponding duty cycle
+* @param temp the temperature used to set the corresponding duty cycle
+* the piecewise linear function was found by characterizing the previously open-loop system at incremented duty cycle values
+*
+**/
 void setDutyCycle(int temp){
     if(temp > 76){
         setPWM(0);
@@ -95,6 +127,11 @@ void setDutyCycle(int temp){
     }
 }
 
+/**
+* setPWM sets the PWM duty cycle corresponding to a duty cycle percentage input
+* @param bitDuty the duty cycle to set the PWM to. Multiplies by 10 since CCR0 is 1000. 
+*
+**/
 void setPWM(unsigned int bitDuty){
     if(bitDuty>100){
         bitDuty = 100;
@@ -103,56 +140,83 @@ void setPWM(unsigned int bitDuty){
     TA1CCR1 = bitDuty;
 }
 
+/**
+* pwmUARTInterrupt receives a value over UART and assigns it to the goal temperature. 
+* @param bufInput the value from the RX buffer
+*
+**/
 void pwmUARTInterrupt(long bufInput){
     goaltemp = bufInput;;
 }
 
+/**
+* ADC10Interrupt disables the ADC then builds an array to compute the average of 10 values (achieved in 5 seconds) which is used to update the PWM value according to the error
+* between the goal temperature and the current temperature. Also transmits the average of 10 via UART.
+* 
+**/
 void ADC10Interrupt(){
-    ADC10CTL0 &= ~ENC;                        // ADC10 disabled
-    ADC10CTL0 = 0;                            // ADC10, Vref disabled completely
-//    long temp = ADC10MEM;
-//    temp *= 330;
-//    temp /= 1024;
-//    UCA0TXBUF = temp;                     //send to TX
-    if(tempBuf_index < 10){
+    ADC10CTL0 &= ~ENC;                        			// ADC10 disabled
+    ADC10CTL0 = 0;                            			// ADC10, Vref disabled completely
+	
+    if(tempBuf_index < 10){					  			//receive a new value and put it in the next index location in the array of data
         tempBuf[tempBuf_index] = ADC10MEM;
         tempBuf_index++;
     }
-    else{
+    else{									  			//compute the average of 10 from the array then update the PWM value and output the current temperature
         long average = 0;
         int i = 0;
         for(i = 0; i < 10 ; i ++){
             average += tempBuf[i];
         }
-        average /= 10;
-        long temperature = (average * 330) >> 10;
+        average /= 10;						  			//average computed
+        long temperature = (average * 330) >> 10;		//convert to degrees
         int newtemp = 0;
         long error = 0;
-        error = (goaltemp - temperature);
-        newtemp = (k*error) + goaltemp;
-        setDutyCycle(newtemp);
-        UCA0TXBUF = temperature;
-        tempBuf_index = 0;
+        error = (goaltemp - temperature);				//comparator error signal
+        newtemp = (k*error) + goaltemp;					//COB to calculate updated "goal" temperature, accounting for error via feedback with a gain of k
+        setDutyCycle(newtemp);							//update PWM
+        UCA0TXBUF = temperature;						//transmit current temperature
+        tempBuf_index = 0;								//reset data collection
     }
 }
 
+/**
+* timerA0 interrupt turns the ADC back on to collect another data point
+* this triggers at 2Hz.
+*
+**/
 void timerA0Interrupt(){
     ADC10CTL0 = SREF_0 + ADC10SHT_2 + ADC10ON + ADC10IE;
     ADC10CTL0 |= ENC;                         // ADC10 enable set
 }
 
+/**
+* UART RX ISR
+* updates the goal temperature. 
+*
+**/
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void){
     pwmUARTInterrupt(UCA0RXBUF);
 }
-// ADC10 interrupt service routine
+
+/**
+* ADC10 ISR
+* computes average of 10, updates PWM, transmits current temperature, turns off ADC. 
+*
+**/
 #pragma vector=ADC10_VECTOR
 __interrupt void ADC10_ISR(void)
 
 {
     ADC10Interrupt();
 }
-// Timer A0 interrupt service routine
+
+/**
+* Timer A0 ISR
+* turns the ADC on
+*
+**/
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A(void)
 
